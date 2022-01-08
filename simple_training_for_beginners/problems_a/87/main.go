a9f8bb5ce22f87ca83b038b9e2ab894145dc21ea
sultanaliev-s <sultanaliev_s@auca.kg>
2022-01-08 20:39:26 +0600

    Solve codeforces/1081A

---
+++ simple_training_for_beginners/problems_a/87/main.go
@@ -0,0 +1,15 @@
+// https://codeforces.com/problemset/problem/1081/A
+
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	if n == 2 {
+		fmt.Println(2)
+	} else {
+		fmt.Println(1)
+	}
+}
